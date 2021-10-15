90fdc86fee1fc8d89c1b0bfcd33723e03e6d2b2c
Jschles1 <jschles1@ramapo.edu>
2021-10-14 20:10:56 -0400

    Update contract for IPFS

--- contracts/TextBox.sol
+++ contracts/TextBox.sol
@@ -7,9 +7,6 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-// We need to import the helper functions from the contract that we copy/pasted.
-import { Base64 } from "./libraries/Base64.sol";
-
 import "hardhat/console.sol";
 
 // We inherit the contract we imported. This means we'll have access
@@ -19,278 +16,37 @@ contract TextBox is ERC721URIStorage {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
-  // This is our SVG code. All we need to change is the word that's displayed. Everything else stays the same.
-  // So, we make a baseSvg variable here that all our NFTs can use.
-  // string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
-
-  // We split the SVG at the part where it asks for the background color.
-  string svgPartOne = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='";
-  string svgPartTwo = "'/><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
-
-  // I create three arrays, each with their own theme of random words.
-  // Pick some random funny words, names of anime characters, foods you like, whatever! 
-  string[] firstWords = [
-    "Common", "Uncommon", "Rare", "Epic", "Legendary", "Mythic"
-  ];
-  string[] secondWords = [
-    "Jolly", "Lazy", "Nosy", "Aloof", "Rude", "Calm", 
-    "Charming", "Famous", "Funny", "Happy", "Sad", "Innocent",
-    "Healthy", "Excited", "Bright", "Dark", "Odd", "Plain",
-    "Super", "Jealous", "Frail", "Large", "Small"
-  ];
-  string[] thirdWords = [
-    "Ninja", "Engineer", "Dragon", "Gnome", "Human", "Soldier",
-    "Athlete", "Harambe", "Bull", "Bear", "Platypus", "Vampire",
-    "Octopus", "Reptile", "Duck", "Parrot", "Owl", "Cat",
-    "Zombie", "Punk", "Ape", "Friend", "Warlock", "Wizard", "Hamburger"
-  ];
-
-  // Get fancy with it! Declare a bunch of colors.
-  string[] colors = [
-    "aliceblue",
-    "antiquewhite",
-    "aqua",
-    "aquamarine",
-    "azure",
-    "beige",
-    "bisque",
-    "black",
-    "blanchedalmond",
-    "blue",
-    "blueviolet",
-    "brown",
-    "burlywood",
-    "cadetblue",
-    "chartreuse",
-    "chocolate",
-    "coral",
-    "cornflowerblue",
-    "cornsilk",
-    "crimson",
-    "cyan",
-    "darkblue",
-    "darkcyan",
-    "darkgoldenrod",
-    "darkgray",
-    "darkgreen",
-    "darkgrey",
-    "darkkhaki",
-    "darkmagenta",
-    "darkolivegreen",
-    "darkorange",
-    "darkorchid",
-    "darkred",
-    "darksalmon",
-    "darkseagreen",
-    "darkslateblue",
-    "darkslategray",
-    "darkslategrey",
-    "darkturquoise",
-    "darkviolet",
-    "deeppink",
-    "deepskyblue",
-    "dimgray",
-    "dimgrey",
-    "dodgerblue",
-    "firebrick",
-    "floralwhite",
-    "forestgreen",
-    "fuchsia",
-    "gainsboro",
-    "ghostwhite",
-    "gold",
-    "goldenrod",
-    "gray",
-    "green",
-    "greenyellow",
-    "grey",
-    "honeydew",
-    "hotpink",
-    "indianred",
-    "indigo",
-    "ivory",
-    "khaki",
-    "lavender",
-    "lavenderblush",
-    "lawngreen",
-    "lemonchiffon",
-    "lightblue",
-    "lightcoral",
-    "lightcyan",
-    "lightgoldenrodyellow",
-    "lightgray",
-    "lightgreen",
-    "lightgrey",
-    "lightpink",
-    "lightsalmon",
-    "lightseagreen",
-    "lightskyblue",
-    "lightslategray",
-    "lightslategrey",
-    "lightsteelblue",
-    "lightyellow",
-    "lime",
-    "limegreen",
-    "linen",
-    "magenta",
-    "maroon",
-    "mediumaquamarine",
-    "mediumblue",
-    "mediumorchid",
-    "mediumpurple",
-    "mediumseagreen",
-    "mediumslateblue",
-    "mediumspringgreen",
-    "mediumturquoise",
-    "mediumvioletred",
-    "midnightblue",
-    "mintcream",
-    "mistyrose",
-    "moccasin",
-    "navajowhite",
-    "navy",
-    "oldlace",
-    "olive",
-    "olivedrab",
-    "orange",
-    "orangered",
-    "orchid",
-    "palegoldenrod",
-    "palegreen",
-    "paleturquoise",
-    "palevioletred",
-    "papayawhip",
-    "peachpuff",
-    "peru",
-    "pink",
-    "plum",
-    "powderblue",
-    "purple",
-    "red",
-    "rosybrown",
-    "royalblue",
-    "saddlebrown",
-    "salmon",
-    "sandybrown",
-    "seagreen",
-    "seashell",
-    "sienna",
-    "silver",
-    "skyblue",
-    "slateblue",
-    "slategray",
-    "slategrey",
-    "snow",
-    "springgreen",
-    "steelblue",
-    "tan",
-    "teal",
-    "thistle",
-    "tomato",
-    "turquoise",
-    "violet",
-    "wheat",
-    "white",
-    "whitesmoke",
-    "yellow",
-    "yellowgreen"
-  ];
-
   uint maxMints = 250;
   uint totalNFTsMinted = 0;
 
   event NewEpicNFTMinted(address sender, uint256 tokenId);
 
   // We need to pass the name of our NFTs token and it's symbol.
-  constructor() ERC721("TextBoxes", "TEXTBOX")  {
+  constructor() ERC721("TextBox NFT Collection", "TEXTBOX")  {
     console.log('Textbox Contract deployed.');
   }
 
-  // I create a function to randomly pick a word from each array.
-  function pickRandomFirstWord(uint256 tokenId) public view returns (string memory) {
-    // I seed the random generator. More on this in the lesson. 
-    uint256 rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));
-    // Squash the # between 0 and the length of the array to avoid going out of bounds.
-    rand = rand % firstWords.length;
-    return firstWords[rand];
-  }
-
-  function pickRandomSecondWord(uint256 tokenId) public view returns (string memory) {
-    uint256 rand = random(string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId))));
-    rand = rand % secondWords.length;
-    return secondWords[rand];
-  }
-
-  function pickRandomThirdWord(uint256 tokenId) public view returns (string memory) {
-    uint256 rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId))));
-    rand = rand % thirdWords.length;
-    return thirdWords[rand];
-  }
-
-  function pickRandomColor(uint256 tokenId) public view returns (string memory) {
-    uint256 rand = random(string(abi.encodePacked("COLOR", Strings.toString(tokenId))));
-    rand = rand % colors.length;
-    return colors[rand];
-  }
-
-  function random(string memory input) internal pure returns (uint256) {
-    return uint256(keccak256(abi.encodePacked(input)));
-  }
-
   // A function our user will hit to get their NFT.
-  function makeAnEpicNFT() public {
+  function makeAnEpicNFT(string memory metadataURI) public {
     console.log('About to make an epic NFT!');
     uint256 newItemId = _tokenIds.current();
     require(totalNFTsMinted != maxMints, "Sorry, max NFT's have been minted.");
-
-    // We go and randomly grab one word from each of the three arrays.
-    string memory first = pickRandomFirstWord(newItemId);
-    string memory second = pickRandomSecondWord(newItemId);
-    string memory third = pickRandomThirdWord(newItemId);
-    string memory combinedWord = string(abi.encodePacked(first, second, third));
-
-    // I concatenate it all together, and then close the <text> and <svg> tags.
-    // Add the random color in.
-    string memory randomColor = pickRandomColor(newItemId);
-    string memory finalSvg = string(abi.encodePacked(svgPartOne, randomColor, svgPartTwo, combinedWord, "</text></svg>"));
-
-    // Get all the JSON metadata in place and base64 encode it.
-    string memory json = Base64.encode(
-      bytes(
-        string(
-          abi.encodePacked(
-            '{"name": "',
-            // We set the title of our NFT as the generated word.
-            combinedWord,
-            '", "description": "A highly acclaimed collection of squares.", "image": "data:image/svg+xml;base64,',
-            // We add data:image/svg+xml;base64 and then append our base64 encode our svg.
-            Base64.encode(bytes(finalSvg)),
-            '"}'
-          )
-        )
-      )
-    );
-
-    // Just like before, we prepend data:application/json;base64, to our data.
-    string memory finalTokenUri = string(
-      abi.encodePacked("data:application/json;base64,", json)
-    );
-
-    console.log("\n--------------------");
-    console.log(finalTokenUri);
-    console.log("--------------------\n");
 
     // Mint NFT to the sender
     _safeMint(msg.sender, newItemId);
   
     // We'll be setting the tokenURI later!
-    _setTokenURI(newItemId, finalTokenUri);
+    _setTokenURI(newItemId, metadataURI);
   
     // Change NFT ID for next person
     _tokenIds.increment();
     totalNFTsMinted++;
     console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
     emit NewEpicNFTMinted(msg.sender, newItemId);
+  }
+
+  function _baseURI() internal pure override returns (string memory) {
+    return "ipfs://";
   }
 
   function getTotalNFTsMinted() public view returns (uint) {
